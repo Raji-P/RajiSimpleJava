9fe7a557efb8a8acc5c316b84370b34fc681b621
Raji-P <sruthi.1418it@gmail.com>
2021-09-03 12:15:47 +0530

    second commit

--- product-application/src/main/java/com/product/productapplication/ProductApplication.java
+++ product-application/src/main/java/com/product/productapplication/ProductApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class
 
-ProductApplication {
+ProductApplication       {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
